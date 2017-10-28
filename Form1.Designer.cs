9c8ef92cc85affc5d6ded2b06457ee389119f242
Erik Lammers <erik.lammers93@gmail.com>
2017-10-27 20:35:24 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Audivel
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.audioChannelLeveler = new System.ComponentModel.BackgroundWorker();
+            this.primarySource = new System.Windows.Forms.ComboBox();
+            this.secondarySource = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.changeDelayBox = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.reductionBox = new System.Windows.Forms.NumericUpDown();
+            this.iAudioSessionControl2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.iAudioSessionControl2BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.changeDelayBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reductionBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iAudioSessionControl2BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iAudioSessionControl2BindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // audioChannelLeveler
+            // 
+            this.audioChannelLeveler.DoWork += new System.ComponentModel.DoWorkEventHandler(this.audioChannelLeveler_DoWork);
+            // 
+            // primarySource
+            // 
+            this.primarySource.FormattingEnabled = true;
+            this.primarySource.Location = new System.Drawing.Point(12, 25);
+            this.primarySource.Name = "primarySource";
+            this.primarySource.Size = new System.Drawing.Size(260, 21);
+            this.primarySource.TabIndex = 0;
+            this.primarySource.DropDown += new System.EventHandler(this.updateSessionList);
+            this.primarySource.SelectedIndexChanged += new System.EventHandler(this.primarySource_SelectedIndexChanged);
+            // 
+            // secondarySource
+            // 
+            this.secondarySource.FormattingEnabled = true;
+            this.secondarySource.Location = new System.Drawing.Point(12, 228);
+            this.secondarySource.Name = "secondarySource";
+            this.secondarySource.Size = new System.Drawing.Size(260, 21);
+            this.secondarySource.TabIndex = 1;
+            this.secondarySource.Visible = false;
+            this.secondarySource.DropDown += new System.EventHandler(this.updateSessionList);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Audio Source";
+            // 
+            // changeDelayBox
+            // 
+            this.changeDelayBox.Increment = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.changeDelayBox.Location = new System.Drawing.Point(12, 69);
+            this.changeDelayBox.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.changeDelayBox.Name = "changeDelayBox";
+            this.changeDelayBox.Size = new System.Drawing.Size(96, 20);
+            this.changeDelayBox.TabIndex = 3;
+            this.changeDelayBox.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Change Delay (ms)";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(197, 52);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(197, 81);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 6;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Reduction %";
+            // 
+            // reductionBox
+            // 
+            this.reductionBox.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.reductionBox.Location = new System.Drawing.Point(114, 69);
+            this.reductionBox.Name = "reductionBox";
+            this.reductionBox.Size = new System.Drawing.Size(73, 20);
+            this.reductionBox.TabIndex = 10;
+            this.reductionBox.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // iAudioSessionControl2BindingSource
+            // 
+            this.iAudioSessionControl2BindingSource.DataSource = typeof(Audivel.IAudioSessionControl2);
+            // 
+            // iAudioSessionControl2BindingSource1
+            // 
+            this.iAudioSessionControl2BindingSource1.DataSource = typeof(Audivel.IAudioSessionControl2);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 109);
+            this.Controls.Add(this.reductionBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.changeDelayBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondarySource);
+            this.Controls.Add(this.primarySource);
+            this.Name = "MainWindow";
+            this.Text = "Audivel";
+            ((System.ComponentModel.ISupportInitialize)(this.changeDelayBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reductionBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iAudioSessionControl2BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iAudioSessionControl2BindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker audioChannelLeveler;
+        private System.Windows.Forms.ComboBox primarySource;
+        private System.Windows.Forms.ComboBox secondarySource;
+        private System.Windows.Forms.BindingSource iAudioSessionControl2BindingSource;
+        private System.Windows.Forms.BindingSource iAudioSessionControl2BindingSource1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown changeDelayBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown reductionBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Audivel
+{
+    public partial class MainWindow : Form
+    {
+        private List<IAudioSessionControl2> sessionList;
+        private IAudioSessionControl2 primarySourceSession = null;
+        private IAudioSessionControl2 secondarySourceSession = null;
+        private GlobalKeyboardHook _globalKeyboardHook;
+        private bool queue = true;
+        private int deltaMS;
+        private DateTime lastKeyTime = DateTime.UtcNow;
+        private List<int> pressedKeys = new List<int>(); 
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //This is basically a keylogger, but I swear it won't save shit.
+            _globalKeyboardHook = new GlobalKeyboardHook();
+            _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+        }
+
+        private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            lastKeyTime = DateTime.UtcNow;
+            Debug.WriteLine(lastKeyTime);
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown && !pressedKeys.Contains(e.KeyboardData.VirtualCode))
+            {
+                pressedKeys.Add(e.KeyboardData.VirtualCode);
+            } else if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp)
+            {
+                pressedKeys.Remove(e.KeyboardData.VirtualCode);
+            }
+        }
+
+        public void Dispose()
+        {
+            _globalKeyboardHook?.Dispose();
+        }
+
+        private void audioChannelLeveler_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (primarySourceSession == null)
+            {
+                return;
+            }
+            Guid guid = Guid.Empty;
+            ISimpleAudioVolume primaryControl = primarySourceSession as ISimpleAudioVolume;
+            float highVolumeLevel;
+            primaryControl.GetMasterVolume(out highVolumeLevel);
+            Console.WriteLine(highVolumeLevel);
+            
+            Console.WriteLine("Doing work.");
+            while (queue)
+            {
+                TimeSpan span = DateTime.UtcNow - lastKeyTime;
+                deltaMS = (int)span.TotalMilliseconds;
+
+                Console.WriteLine(deltaMS);
+
+                if (deltaMS > changeDelayBox.Value && pressedKeys.Count == 0)
+                {
+                    primaryControl.SetMasterVolume(highVolumeLevel * (float)(reductionBox.Value / 100), ref guid);
+                } else
+                {
+                    primaryControl.SetMasterVolume(highVolumeLevel, ref guid);
+                }
+
+                float curVolumeLevel;
+                primaryControl.GetMasterVolume(out curVolumeLevel);
+                Console.WriteLine(curVolumeLevel);
+
+            }
+            Console.WriteLine("Not doing work.");
+            primaryControl.SetMasterVolume(highVolumeLevel, ref guid);
+        }
+
+        private string formatSessionIdentifier(string ident)
+        {
+            if (ident.IndexOf(".exe") != -1)
+            {
+                ident = ident.Remove(ident.IndexOf(".exe"));
+            }
+            if (ident.IndexOf("%b") != -1)
+            {
+                ident = ident.Remove(ident.IndexOf("%b"));
+            }
+            if (ident.LastIndexOf('|') != -1)
+            {
+                ident = ident.Remove(0, ident.LastIndexOf('|') + 1);
+            }
+            if (ident.LastIndexOf('\\') != -1)
+            {
+                ident = ident.Remove(0, ident.LastIndexOf('\\') + 1);
+            }
+
+            return ident;
+        }
+
+        private void updateSessionList(object sender, EventArgs e)
+        {
+            primarySource.Items.Clear();
+            secondarySource.Items.Clear();
+            sessionList = VolumeMixer.GetVolumeSessionList();
+
+            foreach (IAudioSessionControl2 session in sessionList)
+            {
+                if (session.IsSystemSoundsSession() == 0)
+                {
+                    continue;
+                }
+                string sessionName;
+                session.GetSessionIdentifier(out sessionName);
+                sessionName = formatSessionIdentifier(sessionName);
+                primarySource.Items.Add(sessionName);
+                secondarySource.Items.Add(sessionName);
+            }
+        }
+
+        private void primarySource_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (IAudioSessionControl2 session in sessionList)
+            {
+                string sessionName;
+                session.GetSessionIdentifier(out sessionName);
+                sessionName = formatSessionIdentifier(sessionName);
+                if (sessionName.Equals(primarySource.SelectedItem))
+                {
+                    primarySourceSession = session;
+                    Debug.WriteLine(session);
+                    break;
+                }
+            }
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            stopButton.Enabled = true;
+            startButton.Enabled = false;
+            primarySource.Enabled = false;
+            changeDelayBox.Enabled = false;
+            reductionBox.Enabled = false;
+
+            if (!audioChannelLeveler.IsBusy)
+            {
+                queue = true;
+
+                audioChannelLeveler.RunWorkerAsync();
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            stopButton.Enabled = false;
+            startButton.Enabled = true;
+            primarySource.Enabled = true;
+            changeDelayBox.Enabled = true;
+            reductionBox.Enabled = true;
+
+            if (audioChannelLeveler.IsBusy)
+            {
+                queue = false;
+            }
+        }
+
+    }
+}
